8548ce14cd1123b6ddab842486cc9af112fcf5e3
lphillipe <luisphillipe200@gmail.com>
2026-06-11 21:02:54 -0300

    Add menu and conditions

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+
+def main():
+    st.title('DjangoFlix App')
+
+    menu_option = st.sidebar.selectbox(
+        'Selecione uma opção',
+        ['Inicio', 'Gêneros', 'Atores/Atrizes', 'Filmes', 'Avaliações']
+    )
+
+    if menu_option == 'Início':
+        st.write('Início')
+
+    if menu_option == 'Gêneros':
+        st.write('Lista de Gêneros')
+
+    if menu_option == 'Atores/Atrizes':
+        st.write('Lista de Atores/Atrizes')
+
+    if menu_option == 'Filmes':
+        st.write('Lista de Filmes')
+
+    if menu_option == 'Avaliações':
+        st.write('Lista de Avaliações')
+
+if __name__ == '__main__':
+    main() 
