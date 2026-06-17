c452bea56bafa6df041adb56f7ef3f43ef4d56a2
lphillipe <luisphillipe200@gmail.com>
2026-06-17 06:27:16 -0300

    Fix genre list syntax and improve genre display functionality

--- genres/page.py
+++ genres/page.py
@@ -1,4 +1,20 @@
 import streamlit as st
+from st_aggrid import AgGrid
+
+genres = [
+    {
+        'id': 1,
+        'name': 'Ação'
+    }
+    {
+        'id': 2,
+        'name': 'Comedia'
+    }
+    {
+        'id': 3,
+        'name': 'Terror'
+    }
+]
 
 def show_genres():
     st.write('Lista de Gêneros:')
