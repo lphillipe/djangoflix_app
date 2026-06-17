624e9b8a14419584a35df45b29333e7fc73e4060
lphillipe <luisphillipe200@gmail.com>
2026-06-17 07:18:51 -0300

    Fix genre list formatting and update dependencies in project files

--- genres/page.py
+++ genres/page.py
@@ -6,15 +6,15 @@ genres = [
     {
         'id': 1,
         'name': 'Ação'
-    }
+    },
     {
         'id': 2,
         'name': 'Comedia'
-    }
+    },
     {
         'id': 3,
         'name': 'Terror'
-    }
+    },
 ]
 
 def show_genres():
