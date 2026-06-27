ee11157576f4df6acc0bbc1d3f4c59c40f483ef2
lphillipe <luisphillipe200@gmail.com>
2026-06-27 18:49:13 -0300

    Refactor genre retrieval to use GenreService and improve data handling in show_genres function

--- genres/page.py
+++ genres/page.py
@@ -1,30 +1,24 @@
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
+from genres.service import GenreService
 
-genres = [
-    {
-        'id': 1,
-        'name': 'Ação'
-    },
-    {
-        'id': 2,
-        'name': 'Comedia'
-    },
-    {
-        'id': 3,
-        'name': 'Terror'
-    },
-]
 
 def show_genres():
-    st.write('Lista de Gêneros:')
+    genre_service = GenreService()
+    genres = genre_service.get_genres()
 
-    AgGrid(
-        data=pd.DataFrame(genres),
-        reload_data=True,
-        key='genre_grid',
-    )
+    if genres:
+        st.write('Lista de Gêneros:')
+        genres_df = pd.json_normalize(genres)
+
+        AgGrid(
+            data=genres_df,
+            reload_data=True,
+            key='genre_grid',
+        )
+    else:
+        st.warning('Nenhum gênero encontrado.') 
 
     st.title('Cadastrar novo Gênero')
     name = st.text_input('Nome do Gênero')
