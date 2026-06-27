60c547e776d7a3104a505aa4f462da24f52868ee
lphillipe <luisphillipe200@gmail.com>
2026-06-27 17:41:49 -0300

    Add logout function to clear session state and handle unauthorized access in GenreRepository

--- genres/repository.py
+++ genres/repository.py
@@ -1,5 +1,6 @@
 import requests
 import streamlit as st
+from login.service import logout
 
 class GenreRepository:
 
@@ -17,4 +18,7 @@ class GenreRepository:
         )
         if response.status_code == 200:
             return response.json()
+        if response.status_code == 401:
+            logout()
+            return None
         raise Exception(f'Erro ao obter dados da API. Status code: {response.status_code}')
--- login/service.py
+++ login/service.py
@@ -12,3 +12,8 @@ def login(username, password):
     else:
         st.session_state.token = response.get('access')
         st.rerun()
+
+def logout():
+    for key in st.session_state.key():
+        del st.session_state[key]
+    st.rerun()
