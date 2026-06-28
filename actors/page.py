c955db39b3502dc255ab265eac24f37e371ec69c
lphillipe <luisphillipe200@gmail.com>
2026-06-28 19:44:21 -0300

    Enhance actor management with ActorService and ActorRepository integration, including actor registration with birthday and nationality inputs.

--- actors/page.py
+++ actors/page.py
@@ -1,33 +1,46 @@
 import pandas as pd
 import streamlit as st
+from datetime import datetime
 from st_aggrid import AgGrid
-
-actors = [
-    {
-        'id': 1,
-        'name': 'Leonardo Di Caprio'
-    },
-    {
-        'id': 2,
-        'name': 'Chris Rock'
-    },
-    {
-        'id': 3,
-        'name': 'Dwayne Johnson'
-    },
-]
-
+from actors.service import ActorService
 
 def show_actors():
-    st.write('Lista de Atores/Atrizes:')
+    actor_service = ActorService()
+    actors = actor_service.get_actors()
 
-    AgGrid(
-        data=pd.DataFrame(actors),
-        reload_data=True,
-        key='actors_grid',
-    )
+    if actors:
+        st.write('Lista de Atores/Atrizes:')
+        actors_df = pd.json_normalize(actors)
+        AgGrid(
+            data=actors_df,
+            reload_data=True,
+            key='actors_grid',
+        )
+    else:
+        st.warning('Nenhum Ator/Atriz encontrado.')
 
     st.title('Cadastrar novo(a) Ator/Atriz')
     name = st.text_input('Nome do Ator/Atriz')
+    birthday = st.date_input(
+        label='Data de nascimento',
+        value=datetime.today(),
+        min_value=datetime(1600, 1, 1).date(),
+        max_value=datetime.today(),
+        format='DD/MM/YYYY',
+    )
+    nationality_dropdown = ['BRAZIL', 'USA']
+    nationality = st.selectbox(
+        label='Nacionalidade',
+        options=nationality_dropdown,
+    )
+
     if st.button('Cadastrar'):
-        st.success(f'Ator/Atriz "{name}" cadastrado(a) com sucesso!')
+        new_actor = actor_service.create_actors(
+            name=name,
+            birthday=birthday,
+            nationality=nationality,
+        )
+        if new_actor:
+            st.rerun()
+        else:
+            st.error('Error ao cadastrar o(a) Ator/Atriz. Verifiquem os campos')
