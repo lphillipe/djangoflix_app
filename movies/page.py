e5e4d517d9a86e802e9988df57c2b364878661ba
lphillipe <luisphillipe200@gmail.com>
2026-07-01 08:04:09 -0300

    Refactor movie registration page to integrate MovieService for dynamic movie retrieval and enhance user input fields for title, release date, genre, actors, and resume.

--- movies/page.py
+++ movies/page.py
@@ -1,33 +1,68 @@
 import pandas as pd
 import streamlit as st
+from datetime import datetime
 from st_aggrid import AgGrid
-
-movies = [
-    {
-        'id': 1,
-        'name': 'Titanic'
-    },
-    {
-        'id': 2,
-        'name': 'Todo mundo odeia o chris'
-    },
-    {
-        'id': 3,
-        'name': 'Velozes e furiosos 5'
-    },
-]
+from actors.service import ActorService
+from genres.service import GenreService
+from movies.service import MovieService
 
 
 def show_movies():
-    st.write('Lista de Filmes:')
+    movie_service = MovieService()
+    movies = movie_service.get_movies()
 
-    AgGrid(
-        data=pd.DataFrame(movies),
-        reload_data=True,
-        key='movies_grid',
-    )
+    if movies:
+
+        st.write('Lista de Filmes:')
+
+        movies_df = pd.json_normalize(movies)
+        movies_df = movies_df.drop(columns=['actors', 'genre.id'])
+
+        AgGrid(
+            data=movies_df,
+            reload_data=True,
+            key='movies_grid',
+        )
+    else:
+        st.warning('Nenhum filme encontrado.')
 
     st.title('Cadastrar novo filme')
-    name = st.text_input('Nome do filme')
+    
+    title = st.text_input('Título')
+
+    release_date = st.date_input(
+        label='Data de lançamento',
+        value=datetime.today(),
+        min_value=datetime(1800, 1, 1).date(),
+        max_value=datetime.today(),
+        format='DD/MM/YYYY',
+    )
+
+    genre_service = GenreService()
+    genres = genre_service.get_genres()
+    genre_names = {genre['name']: genre['id'] for genre in genres}
+    selected_genre_name = st.selectbox('Gênero', list(genre_names.keys()))
+
+
+
+    actor_service = ActorService()
+    actors = actor_service.get_actors()
+    actor_names = {actor['name']: actor['id'] for actor in actors}
+    selected_actors_names = st.multiselect('Atores/Atrizes', list(actor_names.keys()))
+    selected_actors_ids = [actor_names[name] for name in selected_actors_names]
+
+
+    resume = st.text_area('Resumo')
+
     if st.button('Cadastrar'):
-        st.success(f'Filme "{name}" cadastrado(a) com sucesso!')
+        new_movie = movie_service.create_movie(
+            title=title,
+            release_date=release_date,
+            genre=genre_names[selected_genre_name],
+            actors=selected_actors_ids,
+            resume=resume,
+        )
+        if new_movie:
+            st.rerun()
+        else:
+            st.error('Erro ao cadastrar o filme. Verifique os campos')
