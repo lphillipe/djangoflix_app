fab1fb039162fe32772b0779c45c8b0c02815a61
lphillipe <luisphillipe200@gmail.com>
2026-07-06 22:36:41 -0300

    Add movie statistics display to home page and include Plotly dependency

--- home/page.py
+++ home/page.py
@@ -1,4 +1,17 @@
 import streamlit as st
+from movies.service import MovieService
 
 def show_home():
+    movie_service = MovieService()
+    movie_stats = movie_service.get_movie_stats()
+    
     st.title('Estatísticas de Filmes')
+
+    st.subheader('Total de Filmes Cadastrados:')
+    st.write(movie_stats['total_movies'])
+
+    st.subheader('Total de Avaliações Cadastradas:')
+    st.write(movie_stats['total_reviews'])
+
+    st.subheader('Média Geral de Estrelas nas Avaliações:')
+    st.write(movie_stats['average_stars'])
