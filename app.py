e2892f9f9c56ef61197876ea972432a79412fb5f
lphillipe <luisphillipe200@gmail.com>
2026-07-06 22:26:00 -0300

    Add home page functionality with show_home method for displaying movie statistics

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from actors.page import show_actors
 from genres.page import show_genres
+from home.page import show_home
 from login.page import show_login
 from movies.page import show_movies
 from reviews.page import show_reviews
@@ -19,7 +20,7 @@ def main():
         )
 
         if menu_option == 'Início':
-            st.write('Início')
+            show_home()
 
         if menu_option == 'Gêneros':
             show_genres()
